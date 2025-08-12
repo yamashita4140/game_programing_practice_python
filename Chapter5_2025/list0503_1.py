a3bc1991fca7a883d9d69f8851c6409e135d3742
KazzzTen <tengoo1999@gmail.com>
2025-08-12 10:07:24 +0900

    CUIですごろく(改01) ※プレイヤーとコンピュータの位置を出力できるように変更 ・関数banmenで盤面を出力する ・盤面を30マスとし、左端がスタート、右端がゴールという設定 ・マスを・で表現し、print()内で掛け算をすることで出力を簡略化 ・pl_posがプレーヤーの位置 ・com_posがコンピュータの位置

--- Chapter5_2025/list0503_1.py
+++ Chapter5_2025/list0503_1.py
@@ -1,8 +1,9 @@
 pl_pos = 6  # プレイヤーの位置
+com_pos = 3  # コンピュータの位置
 
 
 def banmen():
     print("・" * (pl_pos - 1) + "P" + "・" * (30 - pl_pos))
-
+    print("・" * (com_pos - 1) + "C" + "・" * (30 - com_pos))
 
 banmen()
