26055b43a5f7f4ece40f16d1489ac51b1aa1ebcc
KazzzTen <tengoo1999@gmail.com>
2025-08-12 11:11:39 +0900

    CUIでアルファベット当て(改03) ※正解入力までにかかった時間を計測できるように変更 ※答えを入力して正解判定できるよう変更 ※リストの中から一文字ランダムで選び、その一文字を取り除いたアルファベット列を作成するように変更 ・リストで使用するアルファベットを定義 ・for文でリストから一文字ずつ取り出し出力

--- Chapter5_2025/list0504_1.py
+++ Chapter5_2025/list0504_1.py
@@ -1,4 +1,5 @@
 import random
+import datetime
 
 ALP = ["A", "B", "C", "D", "E", "F", "G"]
 
@@ -10,9 +11,11 @@ for i in ALP:
         alp = alp + i
 
 print(alp)
-
+st = datetime.datetime.now()  # 開始時間を記録
 ans = input("抜けているアルファベットは?:")
 if ans == r:
     print("正解です!")
+    et = datetime.datetime.now()  # 終了時間を記録
+    print((et - st).seconds)  # 経過時間を計算して出力
 else:
     print("違います!")
