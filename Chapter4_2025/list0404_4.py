7fb8137786f7a0279edcf1e1781c87e1ad8e4c66
KazzzTen <tengoo1999@gmail.com>
2025-08-12 09:23:21 +0900

    randomモジュール ガチャの確率体験(改) 100分の1を何回目で引き当てるかをカウントする
    これをmax_cnt回繰り返して平均値を求める

--- Chapter4_2025/list0404_4.py
+++ Chapter4_2025/list0404_4.py
@@ -1,10 +1,17 @@
 import random
 
-cnt = 0
-while True:
-    r = random.randint(1, 100)
-    print(r)
-    cnt = cnt + 1
-    if r == 77:
-        break
-print(str(cnt) + "回目でレアキャラゲット!!")
+# cnt = 0
+max_cnt = 1000
+sum_cnt = 0
+for i in range(max_cnt):
+    cnt = 0
+    while True:
+        r = random.randint(1, 100)
+        # print(r)
+        cnt = cnt + 1
+        if r == 77:
+            sum_cnt = sum_cnt + cnt
+            break
+    # print(str(cnt) + "回目でレアキャラゲット!!")
+avr = sum_cnt / max_cnt
+print(avr)
